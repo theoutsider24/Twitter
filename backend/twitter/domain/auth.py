d9cab4c60199b0675adfe2202daa3c26ee9a770e
Matthew Coyle <mattcoyle48@gmail.com>
2023-08-29 21:57:39 +0000

    Add token validation and decoding

--- backend/twitter/domain/auth.py
+++ backend/twitter/domain/auth.py
@@ -1,18 +1,21 @@
-from typing import Dict
-
 from jose import jwt
+from pydantic import BaseModel, Field
 
 
 class AuthException(Exception):
     pass
 
 
-def validate_token(token) -> Dict:
+class TokenData(BaseModel):
+    user_id: str = Field(..., alias="https://twitter.com/user_id")
+
+
+def validate_token(token) -> TokenData:
     try:
         payload = jwt.decode(
             token, key=None, options={"verify_signature": False}, audience="core"
         )
-        return payload
+        return TokenData.model_validate(payload)
     except jwt.ExpiredSignatureError as exc:
         raise AuthException("Token has expired") from exc
     except jwt.JWTClaimsError as exc:
--- backend/twitter/interface/rest/main.py
+++ backend/twitter/interface/rest/main.py
@@ -1,11 +1,14 @@
 import json
+from typing import Annotated
 
 import redis
 import sentry_sdk
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
+from twitter.domain.auth import validate_token
 from twitter.domain.repository.tweet_repository import TweetRepositry
 from twitter.interface.settings import Settings
 from twitter.service import TweetService
@@ -43,8 +46,18 @@ async def get_tweets():
     return tweet_service.get_all_tweets()
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user_id(token: Annotated[str, Depends(oauth2_scheme)]):
+    return validate_token(token).user_id
+
+
 @app.post("/tweets")
-async def add_tweet(tweet: TweetCreateRequest):
-    tweet = tweet_service.create_tweet(tweet.text, "1")
+async def add_tweet(
+    tweet: TweetCreateRequest,
+    current_user_id: Annotated[str, Depends(get_current_user_id)],
+):
+    tweet = tweet_service.create_tweet(tweet.text, current_user_id)
     r.publish("events", json.dumps(tweet.as_dict()))
     return tweet
